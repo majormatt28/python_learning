3508fd7d040c66258e6ebcfcd86701d170ccdab0
Matthew E Newton <Elijah2895@hotmail.com>
2021-07-26 14:40:29 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+# birth_year = input('Birth year: ')
+# age = 2021 - int(birth_year)
+# print(age)
+
+
+# user_weight = input("Weight(lbs): ")
+# kilo = int(user_weight) * 0.45
+# print(kilo)
+
+
+# name = 'Jennifer'
+# print(name[1:-1])
+
+# first = 'Matt'
+# last = 'Newton'
+# msg = f'{first} [{last}] is a coder'
+# print(msg)
+
+
+# course = 'Python for Beginners'
+# course.len()
+# print(course.upper())
+
+
+# x = 10 + 3 * 2
+# print(x)
+
+
+# import math
+# print(math.ceil(2.9))
+
+# is_hot = False
+# is_cold = False
+
+# if is_hot:
+#     print("It's a hot day")
+#     print("Drink plenty of water")
+# elif is_cold:
+#     print("It's a cold day")
+#     print("Wear warm clothes")  
+# else:
+#     print("It's a lovely day")
+# print("Enjoy your day")
+
+price = 1000000
+good_credit = True
+
+if good_credit:
+    down_payment = 0.1 * price
+else:
+    down_payment = 0.2 * price
+print(f"Down payment: ${down_payment}")
+
