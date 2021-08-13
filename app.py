cac78729b942b572268a3c8814130963d1ffd639
Matthew <elijah2895@hotmail.com>
2021-08-13 11:58:31 -0400

    keyword arguments

--- app.py
+++ app.py
@@ -300,3 +300,14 @@
 
 # Parameters
 
+# def greet_user(first_name, last_name):
+#     print(f'Hi there {first_name} {last_name}!')
+#     print('Welcome aboard')
+
+
+# print("Start")
+# greet_user("John", "Smith")
+# print("Finish")
+
+# Keyword Arguments
+    
